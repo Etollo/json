6b11a02ad463b1ba50213591c78cfaee1d320d2b
Egor <egor.haritonov5618@gmail.com>
2018-12-15 18:31:23 +0400

    First commit tree

---
+++ tree.js
@@ -0,0 +1,18 @@
+
+//var header = document.querySelector('header');
+var body = document.querySelector('body');
+
+var requestURL = ''
+var tree = JSON.parse(typeB);
+// bodyUl(tree);
+
+// function bodyUl(typeA) {
+//     var myUl = document.createElement('ul');
+//     myUl.textContent = typeA['children'];
+//     body.appendChild(myUl);
+
+//     var myLi = document.createElement('li');
+//     myLi.textContent = typeA['k1'];
+//     body.appendChild(myLi);
+// }
+tree.title;
