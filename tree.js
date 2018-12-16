2c8cbc3018e4a568a09c1c33d7e6338fc1a5e0d4
Egor <egor.haritonov5618@gmail.com>
2018-12-16 12:51:13 +0400

    git commit "Second commit tree"

--- tree.js
+++ tree.js
@@ -1,18 +1,38 @@
 
-//var header = document.querySelector('header');
-var body = document.querySelector('body');
+var header = document.querySelector('header');
+//var body = document.querySelector('body');
 
-var requestURL = ''
-var tree = JSON.parse(typeB);
-// bodyUl(tree);
+var requestURL = 'https://github.com/Etollo/json/blob/master/treeBase.json';
+var request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'text';
+request.send();
 
-// function bodyUl(typeA) {
-//     var myUl = document.createElement('ul');
-//     myUl.textContent = typeA['children'];
-//     body.appendChild(myUl);
+request.onload = function() {
+    var treeBaseText = request.response;
+    var treeBase = JSON.parse(treeBaseText);
+    bodyUl(treeBase);
+};
+function bodyUl(typeA) {
+    var myUl = document.createElement('ul');
+    myUl.textContent = typeA.children;
+    header.appendChild(myUl);
 
-//     var myLi = document.createElement('li');
-//     myLi.textContent = typeA['k1'];
-//     body.appendChild(myLi);
+    var myLi = document.createElement('li');
+    myLi.textContent = typeA.k1;
+    header.appendChild(myLi);
+
+    var myArticle = document.createElement('article');
+    var myList = document.createElement('ul');
+    
+    myArticle.appendChild(myList);
+    
+    section.appendChild(myArticle);
+}
+// function showTree(typeA) {
+//     var trees = typeA.title;
+
+//     for (var i = 0; i < trees.length; i++) {
+//         var 
+//     }
 // }
-tree.title;
