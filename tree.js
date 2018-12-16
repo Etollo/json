5699cad4b799bfe41b0532aa340b395d4dc01b75
Egor <egor.haritonov5618@gmail.com>
2018-12-16 18:26:10 +0400

    Добавил обработчик событий для дерева

--- tree.js
+++ tree.js
@@ -13,6 +13,7 @@ request.onload = function() {
     var treeBaseText = request.response;
     bodyUl(treeBaseText);
     showTree(treeBaseText);
+    tree_toggle(treeBaseText);
 };
 function bodyUl(typeA) {
     var myUl = document.createElement('ul');
@@ -21,10 +22,13 @@ function bodyUl(typeA) {
 
 }
  function showTree(typeA) {
-
     var trees = typeA.children;
+    
     for (var i = 0; i < trees.length; i++){
         var myLi = document.createElement('ul');
+        var myH2 = document.createElement('h2');
+
+        myH2.textContent = trees[i].name;
 
         var childrenK1 = trees[i].k1;
         for (var j = 0; j < childrenK1.length; j++){
@@ -32,26 +36,34 @@ function bodyUl(typeA) {
             listItem.textContent = childrenK1[j];
             myLi.appendChild(listItem);
         }
+        header.appendChild(myH2);
         header.appendChild(myLi);
-    }
-//     var trees = typeA.children;
-
-//     for (var i = 0; i < trees.length; i++) {
-//         var myArticle = document.createElement('article');
-//         var myList = document.createElement('li');
-//         var myH2 = document.createElement('h2');
-
-//         myH2.textContent = trees[i].children;
-
-//         var superK1 = trees[i].children;
-//         for (var j = 0; j < superK1.length; j++){
-//             var listItem = document.createElement('li');
-//             listItem.textContent = superK1[j];
-//             myList.appendChild(listItem);
-//         }
-//         myArticle.appendChild(myH2);
-//         myArticle.appendChild(myList);
         
-//         section.appendChild(myArticle);
-//     }
+    }
  }
+ function tree_toggle(event) {
+	event = event || window.event;
+	var clickedElem = event.target || event.srcElement;
+
+	if (!hasClass(clickedElem, 'Expand')) {
+		return ;// клик не там
+	}
+
+	// Node, на который кликнули
+	var node = clickedElem.parentNode;
+	if (hasClass(node, 'ExpandLeaf')) {
+		return ;// клик на листе
+	}
+
+	// определить новый класс для узла
+	var newClass = hasClass(node, 'ExpandOpen') ? 'ExpandClosed' : 'ExpandOpen';
+	// заменить текущий класс на newClass
+	// регексп находит отдельно стоящий open|close и меняет на newClass
+	var re =  /(^|\s)(ExpandOpen|ExpandClosed)(\s|$)/;
+	node.className = node.className.replace(re, '$1'+newClass+'$3');
+}
+
+
+function hasClass(elem, className) {
+	return new RegExp("(^|\\s)"+className+"(\\s|$)").test(elem.className);
+}
