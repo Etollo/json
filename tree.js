010be789d3248a814868680e0627561aafcd2312
Egor <egor.haritonov5618@gmail.com>
2018-12-16 21:35:45 +0400

    Добавленный обработчик событий, не работает

--- tree.js
+++ tree.js
@@ -19,51 +19,40 @@ function bodyUl(typeA) {
     var myUl = document.createElement('ul');
     myUl.textContent = typeA.title;
     header.appendChild(myUl);
-
+    
 }
  function showTree(typeA) {
     var trees = typeA.children;
     
     for (var i = 0; i < trees.length; i++){
         var myLi = document.createElement('ul');
-        var myH2 = document.createElement('h2');
 
-        myH2.textContent = trees[i].name;
+        //myLi.id('myUL');
+        myLi.classList.add('nested');
+
+        myLi.textContent = trees[i].name;
 
         var childrenK1 = trees[i].k1;
         for (var j = 0; j < childrenK1.length; j++){
             var listItem = document.createElement('li');
+            
+
             listItem.textContent = childrenK1[j];
             myLi.appendChild(listItem);
+
+            
+            //listItem.classList.add('caret');
         }
-        header.appendChild(myH2);
         header.appendChild(myLi);
         
     }
  }
- function tree_toggle(event) {
-	event = event || window.event;
-	var clickedElem = event.target || event.srcElement;
-
-	if (!hasClass(clickedElem, 'Expand')) {
-		return ;// клик не там
-	}
-
-	// Node, на который кликнули
-	var node = clickedElem.parentNode;
-	if (hasClass(node, 'ExpandLeaf')) {
-		return ;// клик на листе
-	}
-
-	// определить новый класс для узла
-	var newClass = hasClass(node, 'ExpandOpen') ? 'ExpandClosed' : 'ExpandOpen';
-	// заменить текущий класс на newClass
-	// регексп находит отдельно стоящий open|close и меняет на newClass
-	var re =  /(^|\s)(ExpandOpen|ExpandClosed)(\s|$)/;
-	node.className = node.className.replace(re, '$1'+newClass+'$3');
-}
-
-
-function hasClass(elem, className) {
-	return new RegExp("(^|\\s)"+className+"(\\s|$)").test(elem.className);
-}
+ var toggler = document.getElementsByClassName("caret");
+ 
+ for (var i = 0; i < toggler.length; i++) {
+   toggler[i].addEventListener("click", function() {
+     this.parentElement.querySelector(".nested").classList.toggle("active");
+     this.classList.toggle("caret-down");
+   });
+ }
+ 
