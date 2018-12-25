eb3179a708a567a78472c43d66c008e3878e2a3e
Egor <egor.haritonov5618@gmail.com>
2018-12-25 16:02:33 +0400

    Изменил название в JSON

--- tree.js
+++ tree.js
@@ -16,48 +16,41 @@ request.onload = function() {
     tree_toggle(treeBaseText);
     hasClass(treeBaseText);
 };
-// function bodyUl(typeA) {
-    
-//     myUl.classList.add('Container');
-//     //myUl.id = "myUL";
-// }
-
-// function frameset(typeA) {
-//     var frame = document.createElement('frame');
-//     frame.appendChild(srcElement);
-// }
 function showTree(typeA) {
     var myUl = document.createElement('ul');
     myUl.textContent = typeA.title;
     header.appendChild(myUl);
     var trees = typeA.children;
     myUl.classList.add('Container');
+
     for (var i = 0; i < trees.length; i++){
         var myLi = document.createElement('ul');
 
-        
-        myLi.classList.add('ContainerInside');
+        // myLi.classList.add('ContainerInside');
 
         myLi.textContent = trees[i].name;
-
+        myUl.appendChild(myLi);
         var childrenK1 = trees[i].k1;
+
         for (var j = 0; j < childrenK1.length; j++){
             var listItem = document.createElement('li');
+            // Обрабатывает объект sedan 
             listItem.textContent = childrenK1[j].sedan;
-
             var sedan = childrenK1[j].k1;
+            // Обрабатывает массив k1 
             for (var k = 0; k < sedan.length; k++){
                 var info = document.createElement('ul');
-                info.textContent = sedan[k];
                 listItem.appendChild(info);
+                info.textContent = sedan[k];
+                
             }
+            
             var a = document.createElement('a');
 
-            listItem.classList.add('Node');
-            listItem.classList.add('IsRoot');
+            // listItem.classList.add('Node');
+            // listItem.classList.add('IsRoot');
             listItem.classList.add('ExpandOpen');
 
-            
             var div = document.createElement('div');
             div.classList.add('Expand');
             listItem.appendChild(div);
@@ -79,13 +72,11 @@ function showTree(typeA) {
 	if (!hasClass(clickedElem, 'Expand')) {
 		return; // клик не там
 	}
-
 	// Node, на который кликнули
 	var node = clickedElem.parentNode;
 	if (hasClass(node, 'ExpandLeaf')) {
 		return; // клик на листе
 	}
-
 	// определить новый класс для узла
 	var newClass = hasClass(node, 'ExpandOpen') ? 'ExpandClosed' : 'ExpandOpen';
 	// заменить текущий класс на newClass
@@ -98,14 +89,4 @@ function hasClass(elem, className) {
 	return new RegExp("(^|\\s)"+className+"(\\s|$)").test(elem.className);
 }
 
-
-
-// var toggler = document.getElementsByClassName("caret");
-    
-// for (var i = 0; i < toggler.length; i++) {
-//     toggler[i].addEventListener("click", function() {
-//         this.parentElement.querySelector(".nested").classList.toggle("active");
-//         this.classList.toggle("caret-down");
-//     });
-// }
 
